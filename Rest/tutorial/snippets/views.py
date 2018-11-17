4dfd3972f5917a8580dfdebd0da2339f1c34036b
Nam-kyu <skarb7746@gmail.com>
2018-11-17 20:11:40 +0900

    Django Rest Swagger

--- Rest/tutorial/snippets/views.py
+++ Rest/tutorial/snippets/views.py
@@ -1,13 +1,9 @@
 from snippets.models import Snippets
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework import mixins
-from rest_framework import generics
 from rest_framework import permissions
-from rest_framework.decorators import api_view
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import viewsets
 from django.contrib.auth.models import User
